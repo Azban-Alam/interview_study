de0edd532647dc4ef00227c23d2b8b0cb214bce0
Azban-Alam <alam.israr@gmail.com>
2026-04-30 04:27:33 +0530

    30-April-26 Collections Class Commit

--- src/main/java/Collections_Methods/Collections_Disjoint.java
+++ src/main/java/Collections_Methods/Collections_Disjoint.java
@@ -1,14 +1,15 @@
 package Collections_Methods;
+
 import java.util.*;
 
 public class Collections_Disjoint {
-    public static void main(String[] args) {
-        List<String> list1 = Arrays.asList("A", "B", "C");
-        List<String> list2 = Arrays.asList("X", "Y", "Z");
-        
-        boolean disjoint = Collections.disjoint(list1, list2);
+	public static void main(String[] args) {
+		
+		List<String> list1 = Arrays.asList("A", "B", "C");
+		List<String> list2 = Arrays.asList("X", "Y", "Z");
 
-        // Checks if two collections have no elements in common
-        System.out.println("Disjoint? " + Collections.disjoint(list1, list2));
-    }
+		boolean disjoint = Collections.disjoint(list1, list2);
+		System.out.println(disjoint); // Checks if two collections have no elements in common
+
+	}
 }
---
+++ src/main/java/Collections_Methods/Collections_Min.java
@@ -0,0 +1,13 @@
+package Collections_Methods;
+
+import java.util.*;
+
+public class Collections_Min {
+	public static void main(String[] args) {
+		
+		List<Integer> nums = Arrays.asList(10, 20, 5, 20, 20, 40);
+		Integer min = Collections.min(nums);
+		System.out.println("Min: " + min); // Finds minimum element
+
+	}
+}
