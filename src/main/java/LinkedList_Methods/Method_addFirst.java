9a7c54f65f109964fb582e6303a950e9358d6e81
Azban-Alam <alam.israr@gmail.com>
2026-05-18 11:34:04 +0530

    18-May-26 Commit

---
+++ src/main/java/LinkedList_Methods/Method_addFirst.java
@@ -0,0 +1,15 @@
+package LinkedList_Methods;
+import java.util.LinkedList;
+
+public class Method_addFirst {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    
+    // Use addFirst() to add the item to the beginning
+    cars.addFirst("Mazda");
+    System.out.println(cars);
+  }
+}
---
+++ src/main/java/LinkedList_Methods/removeFirst.java
@@ -0,0 +1,16 @@
+package LinkedList_Methods;
+import java.util.LinkedList;
+
+public class removeFirst {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    
+    // Use removeFirst() remove the first item from the list
+    cars.removeFirst();
+    System.out.println(cars);
+  }
+}
